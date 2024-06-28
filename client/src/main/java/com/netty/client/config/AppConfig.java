62a72f3ac8b767175615a57d96beae4bd4affe87
wq0angle <wq01862845@alibaba-inc.com>
2024-06-28 16:52:54 +0800

    配置项优化

--- client/src/main/java/com/netty/client/config/AppConfig.java
+++ client/src/main/java/com/netty/client/config/AppConfig.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Component;
 
 @Configuration
-@PropertySource("file:${user.dir}/proxy.properties")
+@PropertySource("file:${user.dir}/proxy-client.properties")
 @Data
 @Component
 public class AppConfig {
@@ -23,25 +23,4 @@ public class AppConfig {
 
     @Value("${ssl.request.enabled}")
     private Boolean sslRequestEnabled;
-
-    @Value("${server.port}")
-    private Integer serverPort;
-
-    @Value("${ssl.listener.enabled}")
-    private Boolean sslListenerEnabled;
-
-    @Value("${website.directory}")
-    private String websiteDirectory;
-
-    @Value("${website.port}")
-    private Integer websitePort;
-
-    @Value("${ssl.jks.path}")
-    private String sslJksPath;
-
-    @Value("${ssl.jks.file.password}")
-    private String sslJksFilePassword;
-
-    @Value("${sin.default.file}")
-    private String sinDefaultFile;
 }
